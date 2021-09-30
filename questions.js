acf3195c09e993162bf20abf93982fb0e0359941
Bhargavibhatia215 <66355146+Bhargavibhatia215@users.noreply.github.com>
2021-09-30 10:18:39 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,137 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What’s the name of Joey’s penguin?",
+    answer: "Hugsy",
+    options: [
+      "Snowflake",
+      "Waddle",
+      "Hugsy",
+      "Bobber"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is Chandler’s middle name?",
+    answer: "Muriel",
+    options: [
+      "Muriel",
+      "Jason",
+      "Kim",
+      "Zachary"
+
+    ]
+  },
+    {
+    numb: 3,
+    question: "What kind of uniform does Joey wear to Monica and Chandler’s wedding?",
+    answer: "Soldier",
+    options: [
+      "Chef",
+      "Soldier",
+      "Fire Fighter",
+      "A baseball player"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Monica and Chandler met on Thanksgiving in 1987. She pursued her career as a chef because Chandler complimented her on which dish?",
+    answer: "Mac and cheese",
+    options: [
+      "Green bean casserole",
+      "Meatloaf",
+      "Stuffing",
+      "Mac and cheese"
+    ]
+  },
+    {
+    numb: 5,
+    question: "How many sisters does Joey have ?",
+    answer: "7",
+    options: [
+      "7",
+      "5",
+      "2",
+      "None"
+    ]
+  },
+   {
+    numb: 6,
+    question: "What song does Ross’ monkey, Marcel, keep playing?",
+    answer: "‘The Lion Sleeps Tonight‘ by The Tokens",
+    options: [
+      "‘The Lion Sleeps Tonight‘ by The Tokens",
+      "‘Tiny Dancer’ by Elton John ",
+      "‘The Way You Looked Tonight’ by Tony Bennett",
+      "‘Careless Whisper’ by George Michael"
+    ]
+  },
+   {
+    numb: 7,
+    question: "What does Chandler have to do to get Joey to forgive him, after having kissed Cathy?",
+    answer: "Lie in a wooden box for six hours",
+    options: [
+    "Wear blue lipstick",
+    "Lie in a wooden box for six hours",
+    "Furnish their apartment",
+    "Move to Yemen"
+    ]
+  },
+   {
+    numb: 8,
+    question: "What is the ‘Gellar Cup’ made of?",
+    answer: "A troll doll nailed to a two by four",
+    options: [
+      "A 3D woman coming out of a photo frame",
+      "A troll doll nailed to a two by four",
+      "A fruit bowl found in the garbage",
+      "A sock puppet shaped like a bunny"
+    ]
+  },
+   {
+    numb: 9,
+    question: "What is the name of the hairless cat Rachel gets for herself?",
+    answer: "Mrs Whiskerson",
+    options: [
+      "Fluffy",
+      "Mrs Whiskerson",
+      "Chi Chi",
+      "Mozzarella"
+    ]
+  },
+   {
+    numb: 10,
+    question: "What is the next line in the song, “Monica, Monica, have a happy Hanukkah”",
+    answer: "“Saw Santa Claus, he said hello to Ross”",
+    options: [
+    "“And please tell Joey, Christmas will be snowy”",
+    "“Saw Santa Claus, he said hello to Ross”",
+    "“Spin the dreidel, Rachel”",
+    "“Went to the store, sat on Santa’s lap”"
+
+    ]
+  },
+   {
+    numb: 11,
+    question: "What ridiculously expensive gift did Ross get Carol when he fell in love with her?",
+    answer: "A crystal duck",
+    options: [
+      "A shop window brooch",
+      "A crystal duck",
+      "An early edition of her favourite book",
+      "A $500 dollar watch"
+    ]
+  },
+   {
+    numb: 12,
+    question: "Now imagine you live at the supermarket. Who said this ?",
+    answer: "Chandler",
+    options: [
+      "Chandler",
+      "Ross",
+      "Gunther",
+      "Nora Bing"
+    ]
+  }
+  
+];
